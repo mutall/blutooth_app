a27c031936b6a29790e655bbfe0ccefb486a6397
amshel <amshelhack3r@gmail.com>
2019-09-11 11:02:29 +0000

    initial commit

---
+++ app/src/main/java/com/example/bluetooth_share/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.bluetooth_share;
+
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                showToast("settings clicked");
+            case R.id.click:
+                showToast("you have clicked me");
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void connectBluetooth(){
+        //
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+
+    }
+
+    public void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+}
