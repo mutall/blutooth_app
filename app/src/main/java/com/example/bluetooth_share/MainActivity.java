04c1271589e4c65d920ff68e8bf405a3cd638140
amshel <amshelhack3r@gmail.com>
2019-09-11 12:27:30 +0000

    i have created a bluetooth application to pair devices

--- app/src/main/java/com/example/bluetooth_share/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_share/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.bluetooth_share;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.Locale;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "kiserian";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +34,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                connectBluetooth();
             }
         });
     }
@@ -42,8 +51,12 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.action_settings:
                 showToast("settings clicked");
+                break;
             case R.id.click:
                 showToast("you have clicked me");
+                break;
+            default:
+                showToast("Invalid selection");
         }
 
 
@@ -51,8 +64,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void connectBluetooth(){
-        //
+        //create a ne w bluetooth adapter
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+
+        //test if device has bluetooth. if it doesnt exit after 5sec
+        if (adapter == null){
+            showToast("Device doesnt support bluetooth. Exiting application...");
+
+            try {
+                wait(5000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            this.finish();
+        }
+
+        //check if the bluetooth device is enable. if not prompt the user to enable
+        if(!adapter.isEnabled()){
+            Intent enable_bluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enable_bluetooth, 1);
+        }
+        //get all paired devices
+        Set<BluetoothDevice> myPairedDevices = adapter.getBondedDevices();
+
+        if(myPairedDevices.size() > 0){
+            for (BluetoothDevice bt :myPairedDevices){
+                Log.i(TAG, bt.getName());
+                Log.i(TAG, bt.getAddress());
+                Log.i(TAG, bt.getBluetoothClass().toString());
+            }
+        }
 
     }
 
@@ -60,4 +101,12 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK){
+            showToast("bluetooth connected");
+        }else if(resultCode == RESULT_CANCELED){
+            showToast("user cancelled");
+        }
+    }
 }
