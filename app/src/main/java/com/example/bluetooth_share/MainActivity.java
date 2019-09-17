0aa720b4b17a3d705b7f0129027e9c5dac534264
amshel <amshelhack3r@gmail.com>
2019-09-17 05:27:45 +0000

    Implement a broadcast receiver

--- app/src/main/java/com/example/bluetooth_share/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_share/MainActivity.java
@@ -2,10 +2,11 @@ package com.example.bluetooth_share;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,32 +14,51 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, CompoundButton.OnCheckedChangeListener, View.OnClickListener {
     private static final String TAG = "kiserian";
-
-    String name;
-    String address;
-    String btclass;
-
     ListView listView;
     List list;
-    private HashMap hashMap;
     BluetoothAdapter adapter;
+    Switch aSwitch;
+    TextView tview;
+    MyAdapter adp;
+    Button paired;
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(adapter.ACTION_STATE_CHANGED)) {
+                int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, 0);
+                switch (state) {
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "State on");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "state turning on...");
+                        break;
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "state off");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "state turning off..");
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,98 +66,114 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        aSwitch = findViewById(R.id.switch1);
         listView = findViewById(R.id.listview);
+        tview = findViewById(R.id.textView);
+        paired = findViewById(R.id.paired_btn);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Object random = parent.getItemAtPosition(position);
-                BluetoothDevice device = (BluetoothDevice) random;
-                new ServerBt(adapter);
-                new ClientBt(device, adapter);
-            }
-        });
-        list = new ArrayList();
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                connectBluetooth();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_settings:
-                showToast("settings clicked");
-                break;
-            case R.id.click:
-                showToast("you have clicked me");
-                break;
-            default:
-                showToast("Invalid selection");
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void connectBluetooth(){
         //create a new bluetooth adapter
         adapter = BluetoothAdapter.getDefaultAdapter();
 
+        if(adapter.isEnabled()){
+            paired.setEnabled(true);
+            tview.setText("bluetooth enabled");
+            aSwitch.setChecked(true);
+        }
+
+        paired.setOnClickListener(this);
+        listView.setOnItemClickListener(this);
+        aSwitch.setOnCheckedChangeListener(this);
+
+        list = new ArrayList();
+    }
+
+
+    public void enableBt() {
         //test if device has bluetooth. if it doesn`t exit after 5sec
-        if (adapter == null){
+        if (adapter == null) {
             showToast("Device doesnt support bluetooth. Exiting application...");
 
             try {
                 wait(5000);
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             this.finish();
         }
 
         //check if the bluetooth device is enable. if not prompt the user to enable
-        if(!adapter.isEnabled()){
+        if (!adapter.isEnabled()) {
+            Log.d(TAG, "bluetooth not enabled. enabling...");
             Intent enable_bluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enable_bluetooth, 1);
+            startActivity(enable_bluetooth);
+            IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(receiver, filter);
+            tview.setText("Bluetooth enabled");
+            paired.setEnabled(true);
+        }
+    }
+
+    public void disableBt() {
+        if (adapter.isEnabled()) {
+            adapter.disable();
+            IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(receiver, filter);
+            tview.setText("Bluetooth disabled");
+            paired.setEnabled(false);
+            if(adp != null){
+                adp.clear();
+            }
+        }
+    }
+
+    public void showPairedDevices() {
+        if(adp != null){
+            adp.clear();
         }
         //get all paired devices
         Set<BluetoothDevice> myPairedDevices = adapter.getBondedDevices();
-
-        if(myPairedDevices.size() > 0){
-            for (BluetoothDevice bt :myPairedDevices){
+        if (myPairedDevices.size() > 0) {
+            for (BluetoothDevice bt : myPairedDevices) {
                 list.add(bt);
             }
-            Log.i(TAG, list.toString());
-            MyAdapter adp = new MyAdapter(this, list);
-//            ArrayAdapter adpt = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, list);
+            Log.d(TAG, list.toString());
+            adp = new MyAdapter(MainActivity.this, list);
+            adp.notifyDataSetChanged();
             listView.setAdapter(adp);
         }
 
     }
 
-    public void showToast(String message){
+    public void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK){
-            showToast("bluetooth connected");
-        }else if(resultCode == RESULT_CANCELED){
-            showToast("user cancelled");
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Object random = parent.getItemAtPosition(position);
+        BluetoothDevice device = (BluetoothDevice) random;
+        BluetoothService service = new BluetoothService();
+        service.startClient(device);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            Log.i(TAG, "You toggled on");
+            enableBt();
+        }else {
+            disableBt();
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        showPairedDevices();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 }
